5b2df7c06eed4302e91a7b950508bfa106a43a3f
ashik <ashikagumbe@gmail.com>
2022-05-30 10:15:10 +0530

    first commit

--- usage.py
+++
@@ -1,5 +0,0 @@
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-
-tokenizer = AutoTokenizer.from_pretrained("philschmid/bart-large-cnn-samsum")
-
-model = AutoModelForSeq2SeqLM.from_pretrained("philschmid/bart-large-cnn-samsum")
